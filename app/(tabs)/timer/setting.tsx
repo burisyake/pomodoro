71a18966861d15fb1c8c466172de7585488bc2eb
burisyake <keita.ide78dev@gmail.com>
2025-01-26 22:52:15 +0900

    #13

--- app/(tabs)/timer/setting.tsx
+++ app/(tabs)/timer/setting.tsx
@@ -6,22 +6,35 @@ import { db } from "@/db/db";
 import { settings } from "@/db/schema";
 
 export default function SettingScreen() {
-  const [selectedMinutes, setSelectedMinutes] = useState(25);
-  const [selectedSeconds, setSelectedSeconds] = useState(0);
+  const [pomodoroMinutes, setPomodoroMinutes] = useState(25);
+  const [pomodoroSeconds, setPomodoroSeconds] = useState(0);
+  const [restMinutes, setRestMinutes] = useState(5);
+  const [restSeconds, setRestSeconds] = useState(0);
 
   const saveTime = () => {
-    const totalSeconds = selectedMinutes * 60 + selectedSeconds;
+    const pomodoroTotalSeconds = pomodoroMinutes * 60 + pomodoroSeconds;
+    const restTotalSeconds = restMinutes * 60 + restSeconds;
 
-    if (totalSeconds > 0) {
+    if (pomodoroTotalSeconds > 0 && restTotalSeconds > 0) {
       try {
         db.insert(settings)
           .values({
             key: "pomodoro_time",
-            value: String(totalSeconds),
+            value: String(pomodoroTotalSeconds),
           })
           .onConflictDoUpdate({
             target: settings.key,
-            set: { value: String(totalSeconds) },
+            set: { value: String(pomodoroTotalSeconds) },
+          })
+          .run();
+        db.insert(settings)
+          .values({
+            key: "rest_time",
+            value: String(restTotalSeconds),
+          })
+          .onConflictDoUpdate({
+            target: settings.key,
+            set: { value: String(restTotalSeconds) },
           })
           .run();
         alert("Time setting saved!");
@@ -41,85 +54,126 @@ export default function SettingScreen() {
       value: index,
     }));
 
-  return (
-    <GestureHandlerRootView style={styles.container}>
-      <Text style={styles.text}>Set Timer</Text>
-
-      <View style={styles.pickerContainer}>
-        <Text style={styles.text}>Pomodoro</Text>
-        {/* 分のピッカー */}
-        <RNPickerSelect
-          onValueChange={(value) => setSelectedMinutes(value)}
-          items={generatePickerItems(61)}
-          value={selectedMinutes}
-          placeholder={{}}
-          style={{
-            inputIOS: styles.input,
-            inputAndroid: styles.input,
-          }}
-        />
-        <Text style={styles.colon}>:</Text>
-        {/* 秒のピッカー */}
-        <RNPickerSelect
-          onValueChange={(value) => setSelectedSeconds(value)}
-          items={generatePickerItems(60)}
-          value={selectedSeconds}
-          placeholder={{}}
-          style={{
-            inputIOS: styles.input,
-            inputAndroid: styles.input,
-          }}
-        />
-      </View>
-
-      <TouchableOpacity onPress={saveTime} style={styles.button}>
-        <Text style={styles.buttonText}>Save</Text>
-      </TouchableOpacity>
-    </GestureHandlerRootView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 16,
-  },
-  text: {
-    fontSize: 24,
-    color: "#fff",
-    marginBottom: 16,
-  },
-  pickerContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginVertical: 16,
-  },
-  colon: {
-    fontSize: 24,
-    color: "#fff",
-    marginHorizontal: 8,
-  },
-  button: {
-    backgroundColor: "#696969",
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-  },
-  buttonText: {
-    fontSize: 18,
-    color: "#fff",
-  },
-  input: {
-    fontSize: 24,
-    color: "#fff",
-    paddingVertical: 8,
-    paddingHorizontal: 10,
-    borderBottomWidth: 1,
-    borderColor: "#fff",
-    textAlign: "center",
-    width: 100,
+    return (
+      <GestureHandlerRootView style={styles.container}>
+        <Text style={styles.title}>Set Timer</Text>
+  
+        <View style={styles.rowContainer}>
+          <Text style={styles.label}>Pomodoro</Text>
+          <View style={styles.pickerGroup}>
+            <RNPickerSelect
+              onValueChange={(value) => setPomodoroMinutes(value)}
+              items={generatePickerItems(61)}
+              value={pomodoroMinutes}
+              placeholder={{}}
+              style={{
+                inputIOS: styles.input,
+                inputAndroid: styles.input,
+              }}
+            />
+            <Text style={styles.colon}>:</Text>
+            <RNPickerSelect
+              onValueChange={(value) => setPomodoroSeconds(value)}
+              items={generatePickerItems(60)}
+              value={pomodoroSeconds}
+              placeholder={{}}
+              style={{
+                inputIOS: styles.input,
+                inputAndroid: styles.input,
+              }}
+            />
+          </View>
+        </View>
+  
+        <View style={styles.rowContainer}>
+          <Text style={styles.label}>Rest</Text>
+          <View style={styles.pickerGroup}>
+            <RNPickerSelect
+              onValueChange={(value) => setRestMinutes(value)}
+              items={generatePickerItems(26)}
+              value={restMinutes}
+              placeholder={{}}
+              style={{
+                inputIOS: styles.input,
+                inputAndroid: styles.input,
+              }}
+            />
+            <Text style={styles.colon}>:</Text>
+            <RNPickerSelect
+              onValueChange={(value) => setRestSeconds(value)}
+              items={generatePickerItems(60)}
+              value={restSeconds}
+              placeholder={{}}
+              style={{
+                inputIOS: styles.input,
+                inputAndroid: styles.input,
+              }}
+            />
+          </View>
+        </View>
+  
+        <TouchableOpacity onPress={saveTime} style={styles.button}>
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity>
+      </GestureHandlerRootView>
+    );
   }
-});
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#25292e",
+      justifyContent: "center",
+      padding: 16,
+    },
+    title: {
+      fontSize: 28,
+      color: "#fff",
+      marginBottom: 12,
+      textAlign: "center",
+    },
+    rowContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginVertical: 2,
+    },
+    label: {
+      fontSize: 24,
+      color: "#fff",
+      flex: 1,
+    },
+    pickerGroup: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      flex: 2,
+    },
+    colon: {
+      fontSize: 24,
+      color: "#fff",
+      marginHorizontal: 8,
+    },
+    input: {
+      fontSize: 24,
+      color: "#fff",
+      paddingVertical: 8,
+      paddingHorizontal: 10,
+      borderBottomWidth: 1,
+      borderColor: "#fff",
+      textAlign: "center",
+      width: 100,
+    },
+    button: {
+      backgroundColor: "#696969",
+      paddingVertical: 12,
+      paddingHorizontal: 24,
+      borderRadius: 8,
+      marginTop: 32,
+      alignSelf: "center",
+    },
+    buttonText: {
+      fontSize: 18,
+      color: "#fff",
+    },
+  });
